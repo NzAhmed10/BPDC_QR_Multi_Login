b68327ad2a6ecd823e2a5e0f44b762b9c7793403
NzAhmed10 <109258657+NzAhmed10@users.noreply.github.com>
2025-02-11 22:41:18 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def login_to_lms(account, drivers_list):
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.binary_location = "/usr/bin/google-chrome"
+    options.binary_location = "/usr/bin/google-chrome-stable"
 
     try:
         add_log(f"[{nickname}] Launching ChromeDriver using ChromeDriverManager.")
