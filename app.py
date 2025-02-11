d556e1748749fa4b608caef79c52b6396810318d
NzAhmed10 <109258657+NzAhmed10@users.noreply.github.com>
2025-02-12 00:06:04 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -142,6 +142,7 @@ def login_to_lms(account, drivers_list):
     print(f"drivers_list before append in login_to_lms for {nickname}: {drivers_list}")
 
     options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
@@ -156,8 +157,8 @@ def login_to_lms(account, drivers_list):
         # service = Service(driver_path)
         # driver = webdriver.Chrome(service=service, options=options)
 
-        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
-        driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+        # driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
 
         # Check if driver was launched successfully (basic check)
