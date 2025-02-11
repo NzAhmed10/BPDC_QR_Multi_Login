0e4d9fba9f1ce1275c4f8f5882eba881df153b34
NzAhmed10 <109258657+NzAhmed10@users.noreply.github.com>
2025-02-11 23:56:20 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -146,6 +146,7 @@ def login_to_lms(account, drivers_list):
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--remote-debugging-port=9222")
 
     try:
         add_log(f"[{nickname}] Launching ChromeDriver using ChromeDriverManager.")
