f2d7cd4c03f59bc86d9c0627c27bbada1bbc5d08
NzAhmed10 <109258657+NzAhmed10@users.noreply.github.com>
2025-02-12 00:55:01 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -12,17 +12,16 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-from webdriver_manager.core.os_manager import ChromeType
-from selenium.webdriver.chrome.service import Service as ChromiumService
+# from webdriver_manager.core.os_manager import ChromeType
+# from selenium.webdriver.chrome.service import Service as ChromiumService
 
-from chromedriver_py import binary_path 
+# from chromedriver_py import binary_path 
 
 
 import subprocess  # Import subprocess
@@ -162,12 +161,12 @@ def login_to_lms(account, drivers_list):
         # service = Service(driver_path)
         # driver = webdriver.Chrome(service=service, options=options)
 
-        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
 
         # driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
-        svc = webdriver.ChromeService(executable_path=binary_path)
-        driver = webdriver.Chrome(service=svc)
+        # svc = webdriver.ChromeService(executable_path=binary_path)
+        # driver = webdriver.Chrome(service=svc)
 
 
         # Check if driver was launched successfully (basic check)
