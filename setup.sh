61d8d985070c672a25a68d724dffc1c3c6656dd9
NzAhmed10 <109258657+NzAhmed10@users.noreply.github.com>
2025-02-12 20:01:12 +0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# setup.sh
-# This script sets up the environment for the Streamlit app.
-
-# Make sure the ChromeDriver binary is executable.
-chmod +x chromdriver
-
-# (Optional) Print a message indicating that setup is complete.
-echo "Setup complete: 'chromdriver' is now executable."
