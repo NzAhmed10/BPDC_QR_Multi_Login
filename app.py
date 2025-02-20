baca3ecf7d2839a76feb5638bfe995e9c4c4f290
NzAhmed10 <109258657+NzAhmed10@users.noreply.github.com>
2025-02-20 20:27:58 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def login_to_lms(account, drivers_list):
     # options.add_argument("--headless")
     # options.add_argument("--disable-gpu")
     # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-shm-usage")
     # options.add_argument("--window-size=1920,1080")
     # options.add_argument("--disable-blink-features=AutomationControlled")
 
